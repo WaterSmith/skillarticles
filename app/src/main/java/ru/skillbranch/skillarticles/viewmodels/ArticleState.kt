cbe63dcd9e438398c5d3eabf1c6ef6a3019f645e
Sergey Vodakov <s.vodakov@gmail.com>
2019-12-20 18:41:35 +0200

    First commit

---
+++ app/src/main/java/ru/skillbranch/skillarticles/viewmodels/ArticleState.kt
@@ -0,0 +1,7 @@
+package ru.skillbranch.skillarticles.viewmodels
+
+class ArticleState(
+    var isDarkMode:Boolean  = false,
+    var isBigText:Boolean   = false,
+    var isLike:Boolean      = false,
+    var isBookmark:Boolean  = false)
